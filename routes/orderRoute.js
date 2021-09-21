a6ed490427d4c56ad7c4f0d5a383b6aac91cad6d
Felix Nana Yaw Yeboah - Jefferson <87368189+felixyeboah@users.noreply.github.com>
2021-09-21 11:39:31 +0000

    added new controllers and routes

--- routes/orderRoute.js
+++ routes/orderRoute.js
@@ -4,9 +4,12 @@ const authController = require("../controllers/authController");
 
 const router = express.Router();
 
+router.use(authController.protect);
+router.post("/", orderController.createOrder);
+router.post("/payment-hook", orderController.createPaymentHook);
+
+router.use(authController.restrictTo("admin"));
 router.get("/", orderController.getOrders);
 router.get("/:id", orderController.getOrder);
-router.post("/", authController.protect, orderController.createOrder);
-router.post("/payment-hook", orderController.createPaymentHook);
 
 module.exports = router;
