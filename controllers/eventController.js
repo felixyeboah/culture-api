67f6f97e966a71364ae22ff2efc1fe2e0515b34f
Felix Nana Yaw Yeboah - Jefferson <87368189+felixyeboah@users.noreply.github.com>
2021-09-21 14:40:36 +0000

    added new controllers and routes

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -32,7 +32,7 @@ exports.createEvent = catchAsync(async (req, res, next) => {
   if (!location) return next(new AppError("Location is required!", 400));
   if (!time) return next(new AppError("Time is required!", 400));
 
-  const slug = slugify({ name: date }, { lower: true });
+  const slug = slugify(`${name}${date}`, { lower: true });
 
   console.log("slug", slug);
 
@@ -66,7 +66,7 @@ exports.updateEvent = catchAsync(async (req, res) => {
 
   const { path: cover } = req.file;
 
-  const slug = slugify({ name: date }, { lower: true });
+  const slug = slugify(`${name}${date}`, { lower: true });
 
   let uploadedCover = cloudinary.uploader.upload(cover, {
     resource_type: "auto",
