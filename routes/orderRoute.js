0aeda2bbb9c20b4cc41d2b9e9b932fe45cc7481b
Felix Nana Yaw Yeboah <87368189+felixyeboah@users.noreply.github.com>
2021-12-02 13:54:27 +0000

    added daily sales track endpoint

--- routes/orderRoute.js
+++ routes/orderRoute.js
@@ -5,19 +5,19 @@ const authController = require("../controllers/authController");
 const router = express.Router();
 
 router.post("/payment-hook", orderController.createPaymentHook);
-router.route("/sales").get(orderController.getSales);
-router.route("/send-email").post(orderController.sendQRCode);
-router.route("/generate-ticket").post(orderController.generateQRCode);
-router.route("/pending-orders").delete(orderController.deleteUnsucessfulOrders);
 router.route("/checkout-url").post(orderController.saveCheckoutUrl);
 router.route("/saved-url").post(orderController.getSavedCheckoutUrl);
 router.post("/", orderController.createOrder);
-router.get("/:id", orderController.getOrder);
 
 router.use(authController.protect);
 router.get("/", orderController.getOrders);
 
 router.use(authController.restrictTo("admin"));
+router.route("/sales").get(orderController.getSales);
+router.route("/send-email").post(orderController.sendQRCode);
+router.route("/generate-ticket").post(orderController.generateQRCode);
+router.route("/pending-orders").delete(orderController.deleteUnsucessfulOrders);
+router.route("/:id").get(orderController.getOrder);
 router.route("/:id").delete(orderController.deleteOrder);
 
 module.exports = router;
