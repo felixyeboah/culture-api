9392bbfbfbb258399996099ebf24ad4ef12bc4b1
Felix Nana Yaw Yeboah <87368189+felixyeboah@users.noreply.github.com>
2021-12-11 01:58:33 +0000

    added daily sales track endpoint

--- routes/orderRoute.js
+++ routes/orderRoute.js
@@ -8,16 +8,15 @@ router.post("/payment-hook", orderController.createPaymentHook);
 router.route("/checkout-url").post(orderController.saveCheckoutUrl);
 router.route("/saved-url").post(orderController.getSavedCheckoutUrl);
 router.post("/", orderController.createOrder);
+router.route("/:id").get(orderController.getOrder);
 
 router.use(authController.protect);
+router.use(authController.restrictTo("admin", "checker"));
 router.get("/", orderController.getOrders);
-
-router.use(authController.restrictTo("admin"));
 router.route("/sales").get(orderController.getSales);
 router.route("/send-email").post(orderController.sendQRCode);
 router.route("/generate-ticket").post(orderController.generateQRCode);
 router.route("/pending-orders").delete(orderController.deleteUnsucessfulOrders);
-router.route("/:id").get(orderController.getOrder);
 router.route("/:id").delete(orderController.deleteOrder);
 
 module.exports = router;
